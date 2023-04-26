9d3a28d3d082d811658c5c31622c0f6e5cf21255
HanSeungHyeop <h970409@gmail.com>
2023-04-26 22:50:20 +0900

    Revert "[Feat] order, cart api 추가"
    This reverts commit 1727f15eb2175c629b5a161b0cdd349bc488dbbf.

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-
-router.get('/', (req, res) => {
-  // 프로덕트 등록한거 전부다 조회해서 뿌려주기
-  res.send('This is the root page');
-});
-
-module.exports = router;
--- routes/orderRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-const orderApi = require("../apis/product_api");
-
-router.get("/", orderApi.getAllOrder); // (메인 페이지)
-router.delete("/:order_num", orderApi.deleteOrder); // (메인 페이지)
-
-module.exports = router;
